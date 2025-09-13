8042e5cd739cc69d42972e06f26d55d7f5a31af5
kevindbois <kevin.dbois@iths.se>
2025-09-13 12:22:29 +0200

    fixade några //

--- src/main/java/se/iths/kevin/arrayprojekt/myArrayList.java
+++ src/main/java/se/iths/kevin/arrayprojekt/myArrayList.java
@@ -3,21 +3,21 @@ import java.util.Arrays;
 public class myArrayList {
 
 //    G-delen av uppgiften
-//    public static void printArray(String[] values) {
-//        for (int i = 0; i < values.length; i++) {
-//            System.out.print(values[i] + " ");
-//        }
-//    }
-//
-//    public static void main(String[] args) {
-//    String[] words = {"Hello", "World", "!"};
-//    printArray(words);
-//
+    public static void printArray(String[] values) {
+        for (int i = 0; i < values.length; i++) {
+           System.out.print(values[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+   String[] words = {"Hello", "World", "!"};
+    printArray(words);
 
 
 
 
-//}
+
+}
 
 
     //VG-delen av uppgiften
@@ -36,24 +36,25 @@ public class myArrayList {
     }
     //metod3
     public static void fullName(String lastName, String firstName) {
+
         System.out.println(firstName + " " + lastName);
     }
 
 
 
-    public static void main(String[] args) {
+    //public static void main(String[] args) {
         //anropning av metod1
-        int[] numbers = {9998, 5, 11, 23, 44, 1456, 1112, 1, 9999, 25000};
-        int max = findMax(numbers);
-        System.out.println("Största talet i arrayen: " + max);
+        //int[] numbers = {9998, 5, 11, 23, 44, 1456, 1112, 1, 9999, 25000};
+        //int max = findMax(numbers);
+        //System.out.println("Största talet i arrayen: " + max);
         //
         //anropning metod 2
-        double biggest = bigDouble(5.5, 7.1);
-        System.out.println("Största double: " + biggest);
+        //double biggest = bigDouble(5.5, 7.1);
+        //System.out.println("Största double: " + biggest);
         //anropning metod 3
-        fullName("Dübois", "Kevin");
+        //fullName("Dübois", "Kevin");
 
     }
 
 
-}
+//}
